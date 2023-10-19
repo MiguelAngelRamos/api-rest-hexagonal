19b94c23a282af90512683c78ccfefa42c237fce
Miguel Ramos <return.infox@gmail.com>
2023-10-19 20:42:51 -0300

    vamos probar el servicio getAllStudents

---
+++ documentacion.md
@@ -0,0 +1,13 @@
+# JEST https://jestjs.io/ Para realizar pruebas unitarias.
+
+## Caracteristicas. 
+ - Fácil de configurar.
+ - Cobertura código.
+ - Compatible con varios frameworks (react, Angular y vue)
+
+## Ciclo de vida de Jest
+
+ 1. beforeAll -> Se ejecuta una vez (antes que comiencen todas las pruebas de bloque)
+ 2. beforeEach -> Se ejecuta antes de cada prueba individual en un bloque
+ 3. afterAll -> Se ejecuta una vez que se hayan completado todas las pruebas en un bloque
+ 4. afterEach -> Se ejecuta despues de cada prueba individual en un bloque
---
+++ tests/unit/services/StudentService.test.ts
@@ -0,0 +1,42 @@
+import { StudentService } from '../../../src/application/service/StudentService';
+
+
+const mockStudentRepository = {
+  findAll: jest.fn(),
+  findById: jest.fn(),
+  create: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn()
+  /* jest.fn() es una función de jest que crea un función simulada, conocida como mock, permite rastrear las llamadas a la funcion, simular entornos especificos, adicional nos permite cambiar la implementación en tiempo de ejecución  */
+}
+
+//* Crear un instancia del servicio de estudiantes usando un repositorio simulado
+const studentService = new StudentService(mockStudentRepository);
+
+//* "COMIENZA" un "Bloque de prueba" para el servicio de estudiantes
+
+describe('StudentService', () => {
+  //* Antes de cada prueba del bloque "StudentService", limpia todos los mocks para asegurar que no haya
+  //* interferencias entre pruebas.
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getAllStudents', () => {
+    it('should return all students', async () => {
+      //* Simula que el método findAll del repositorio devuelve una lista de estudiantes
+      mockStudentRepository.findAll.mockResolvedValue([{ id: 1, name: 'Richard', age: 25}, {id: 2, name: 'James', age: 30}]);
+
+      //* Llama al método getAllStudents y almacena el resultado 
+      const result = await studentService.getAllStudents();
+
+      //* Asegura que el resultado tenga dos estudiantes
+      expect(result).toHaveLength(2);
+      //* Aseguro que el primer estudiante tenga el nombre 'Richard'
+      expect(result[0].name).toBe('Richard');
+      //* Aseguro que el segundo estudiante tenga el nombre 'James'
+      expect(result[1].name).toBe('James');
+    });
+  })
+
+});
