3c167bfb5eca9934418e9b570f50e290268a8934
Miguel Ramos <return.infox@gmail.com>
2023-10-19 19:43:36 -0300

    archivo de configuracion

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  
+  preset: 'ts-jest', // preajuste para manejar archivos typescript
+  testEnvironment: 'node', // Especifica el entorno de ejecución donde se ejecutan las pruebas (Node.js)
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Define las extensiones de archivo que jest deberia considerar
+  transform: {
+    
+  }
 }
